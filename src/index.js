ec7aed4758b5a3c048e89ecbf36d5ad4cae94105
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2021-07-20 15:06:14 -0300

    Fix render when using shadow:false

--- src/index.js
+++ src/index.js
@@ -81,6 +81,8 @@ function connectedCallback() {
 		{ ...this._props, context },
 		toVdom(this, this._vdomComponent)
 	);
+
+	this.innerHTML = ""
 	(this.hasAttribute('hydrate') ? hydrate : render)(this._vdom, this._root);
 }
 
